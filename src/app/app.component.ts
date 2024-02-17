55f403e4231ff581f2b3a4af5030fd1a7755ef28
Connie Leung <cleung334@gmail.com>
2024-02-18 00:55:11 +0800

    chore: delete unused files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, VERSION } from '@angular/core';
+import { AppSimpleForm } from './app-simple-form/app-simple-form.component';
+import { AppViewChild } from './app-viewchild/app-viewchild.component';
+import { AppContentChild } from './app-contentchild/app-contentchild.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [AppSimpleForm, AppViewChild, AppContentChild],
+  template: `
+  <header>Angular {{version}}</header>
+  <fieldset>
+    <legend>Enjoy signal queries</legend>
+    <app-simple-form />
+    <app-viewchild />
+    <app-contentchild />
+  </fieldset>`,
 })
 export class AppComponent {
-  title = 'ng-signal-queries-demo';
+  version = VERSION.full;
 }
